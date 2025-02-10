9ade1979d9baa0d8bfc7f95d8e3895aadcb13795
DilaraOztas <bodur-dilara@hotmail.de>
2025-02-10 13:52:46 +0100

    init commit

---
+++ LoveAnimals/LoveAnimalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveAnimalsApp.swift
+//  LoveAnimals
+//
+//  Created by Dilara Öztas on 10.02.25.
+//
+
+import SwiftUI
+
+@main
+struct LoveAnimalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
